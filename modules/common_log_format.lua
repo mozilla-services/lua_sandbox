0017e61d098846578a5c7eb4a53817a25e655df9
Mike Trinkala <trink@acm.org>
2014-03-03 08:22:31 -0800

    Add user agent norimalization code
    https://github.com/mozilla-services/heka/issues/722

--- modules/common_log_format.lua
+++ modules/common_log_format.lua
@@ -9,9 +9,12 @@ local string = require "string"
 local dt = require "date_time"
 local tonumber = tonumber
 local ipairs = ipairs
+local pairs = pairs
 
 local M = {}
 setfenv(1, M) -- Remove external access to contain everything in the module
+
+--[[ Nginx access log grammar generation and parsing --]]
 
 local double = l.digit^1 * "." * l.digit^1
 local msec_time = double / dt.seconds_to_ns
@@ -55,6 +58,67 @@ local function literal_grammar(var)
     return l.P(var)
 end
 
+--[[ User Agent normalization
+Derived from the Persona user agent parser
+https://github.com/mozilla/persona/blob/dev/lib/coarse_user_agent_parser.js
+]]--
+
+-- find the version of the last product in the user agent string
+local function ua_basic(ua)
+    local cur, last = 0, nil
+    while cur do
+        cur = ua:find("/", cur+1)
+        if cur then
+            last = cur
+        end
+    end
+
+    if last then
+        return tonumber(ua:match("^(%d+)", last+1))
+    end
+end
+
+-- find the version of the product identified by the keyword
+local function ua_keyword(kw)
+    return function(ua)
+        local s = ua:find(kw) + kw:len()
+        if s then
+            return tonumber(ua:match("^(%d+)", s))
+        end
+    end
+end
+
+local ua_os_matchers = {
+     ["iPod"]           = "iPod"
+    ,["iPad"]           = "iPad"
+    ,["iPhone"]         = "iPhone"
+    ,["Android"]        = "Android"
+    ,["BlackBerry"]     = "BlackBerry"
+    ,["Linux"]          = "Linux"
+    ,["Macintosh"]      = "Macintosh"
+    ,["FirefoxOS"]      = "Mozilla/5.0 (Mobile; rv:"
+    -- http://en.wikipedia.org/wiki/Microsoft_Windows#Timeline_of_releases
+    ,["Windows 8.1"]    = "Windows NT 6.3"
+    ,["Windows 8"]      = "Windows NT 6.2"
+    ,["Windows 7"]      = "Windows NT 6.1"
+    ,["Windows Vista"]  = "Windows NT 6.0"
+    ,["Windows XP"]     = "Windows NT 5.1"
+    ,["Windows 2000"]   = "Windows NT 5.0"
+}
+
+local ua_browser_matchers = {
+     {"Chrome"        , ua_keyword("Chrome/")}
+    ,{"Opera Mini"    , ua_basic}
+    ,{"Opera Mobile"  , ua_basic}
+    ,{"Opera"         , ua_basic}
+    ,{"MSIE"          , ua_keyword("MSIE ")}
+    ,{"Safari"        , ua_basic}
+    ,{"Firefox"       , ua_basic}
+}
+
+--[[ Public Interface --]]
+
+-- Returns an LPeg grammar based on the Nginx log_format configuration string.
 function build_nginx_grammar(log_format)
     local ws = l.C(l.space^1) / space_grammar
     local variable = l.P"$" * l.C((l.alnum + "_")^1) / nginx_lookup_grammar
@@ -74,6 +138,29 @@ function build_nginx_grammar(log_format)
         end
     end
     return l.Ct(grammar)
+end
+
+-- Returns the user agent browser, version, and os
+function normalize_user_agent(ua)
+    if not ua then return end
+
+    local browser, version, os
+    for k, v in pairs(ua_os_matchers) do
+        if ua:find(v, 1, true) then
+            os = k
+            break
+        end
+    end
+
+    for i, v in ipairs(ua_browser_matchers) do
+        if ua:find(v[1], 1, true) then
+            browser = v[1]
+            version = v[2](ua)
+            break
+        end
+    end
+
+    return browser, version, os
 end
 
 return M
