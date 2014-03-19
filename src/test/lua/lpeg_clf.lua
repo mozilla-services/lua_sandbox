acb552557a6d2ce7a98f1068453c8335376fc2a9
Mike Trinkala <trink@acm.org>
2014-03-19 09:45:06 -0700

    Add strftime grammar builder
    - raise an error on failed grammar construction instead of returning nil

--- src/test/lua/lpeg_clf.lua
+++ src/test/lua/lpeg_clf.lua
@@ -9,7 +9,6 @@ function process(tc)
     if tc == 0 then
         -- common log format
         local grammar = clf.build_nginx_grammar('$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"')
-        if not grammar then return -1 end
         local log = '127.0.0.1 - - [10/Feb/2014:08:46:41 -0800] "GET / HTTP/1.1" 304 0 "-" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0"'
         local fields = grammar:match(log)
         fields.time = fields.time/1e6
@@ -17,7 +16,6 @@ function process(tc)
     elseif tc == 1 then
         -- custom format to test the msec changes
         local grammar = clf.build_nginx_grammar('$msec $remote_addr "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"')
-        if not grammar then return -1 end
         local log = '1391794831.755 127.0.0.1 "GET / HTTP/1.1" 304 0 "-" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0" "-" 0.000'
         local fields = grammar:match(log)
         fields.time = fields.time/1e6
--- src/test/lua/lpeg_syslog.lua
+++ src/test/lua/lpeg_syslog.lua
@@ -8,7 +8,6 @@ function process(tc)
     if tc == 0 then
         -- TraditionalFileFormat
         local grammar = syslog.build_rsyslog_grammar('%TIMESTAMP% %HOSTNAME% %syslogtag%%msg:::sp-if-no-1st-sp%%msg:::drop-last-lf%\n')
-        if not grammar then return -1 end
         local log = 'Feb  9 14:17:01 trink-x230 CRON[20758]: (root) CMD (   cd / && run-parts --report /etc/cron.hourly)\n'
         local fields = grammar:match(log)
         fields.timestamp = fields.timestamp/1e6
@@ -16,7 +15,6 @@ function process(tc)
     elseif tc == 1 then
         -- FileFormat
         local grammar = syslog.build_rsyslog_grammar('%TIMESTAMP:::date-rfc3339% %HOSTNAME% %syslogtag%%msg:::sp-if-no-1st-sp%%msg:::drop-last-lf%\n')
-        if not grammar then return -1 end
         local log = '2014-02-10T08:21:59.111537-08:00 trink-x230 kernel: Kernel logging (proc) stopped.\n'
         local fields = grammar:match(log)
         fields.timestamp = fields.timestamp/1e6
@@ -24,7 +22,6 @@ function process(tc)
     elseif tc == 2 then
         -- ForwardFormat
         local grammar = syslog.build_rsyslog_grammar('<%PRI%>%TIMESTAMP:::date-rfc3339% %HOSTNAME% %syslogtag:1:32%%msg:::sp-if-no-1st-sp%%msg%')
-        if not grammar then return -1 end
         local log = '<6>2014-02-10T08:33:15.407935-08:00 trink-x230 kernel: imklog 5.8.6, log source = /proc/kmsg started.'
         local fields = grammar:match(log)
         fields.timestamp = fields.timestamp/1e6
@@ -32,7 +29,6 @@ function process(tc)
     elseif tc == 3 then
         -- TraditionalForwardFormat
         local grammar = syslog.build_rsyslog_grammar('<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag:1:32%%msg:::sp-if-no-1st-sp%%msg%')
-        if not grammar then return -1 end
         local log = '<6>Feb 10 08:38:47 trink-x230 kernel: imklog 5.8.6, log source = /proc/kmsg started.'
         local fields = grammar:match(log)
         fields.timestamp = fields.timestamp/1e6
@@ -40,7 +36,6 @@ function process(tc)
     elseif tc == 4 then
         -- KitchenSink
         local grammar = syslog.build_rsyslog_grammar("%HOSTNAME% %SOURCE% %FROMHOST% %FROMHOST-IP% %SYSLOGTAG% %PROGRAMNAME% %PRI% %PRI-TEXT% %IUT% %SYSLOGFACILITY% %SYSLOGFACILITY-TEXT% %SYSLOGPRIORITY% %SYSLOGPRIORITY-TEXT% %TIMEGENERATED% %TIMEREPORTED:::date-mysql% %TIMESTAMP:::date-rfc3339% %PROTOCOL-VERSION% %STRUCTURED-DATA% %APP-NAME% %PROCID% %MSGID% %$NOW% %$YEAR% %$MONTH% %$DAY% %$HOUR% %$HHOUR% %$QHOUR% %$MINUTE% %MSG%\n")
-        if not grammar then return -1 end
         local log = 'trink-x230 trink-x230 trink-x230 127.0.0.1 kernel: kernel 6 kern.info<6> 1 0 kern 6 info Feb 10 09:20:53 20140210092053 2014-02-10T09:20:53.559934-08:00 0 - kernel  - imklog 2014-02-10 2014 02 10 09 00 01 20 imklog 5.8.6, log source = /proc/kmsg started.\n'
         local fields = grammar:match(log)
         fields.timestamp = fields.timestamp/1e6
