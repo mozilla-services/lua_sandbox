46caa34747aefefb4b9569c17c756f4efcd55247
Mike Trinkala <trink@acm.org>
2014-05-08 08:25:41 -0700

    Add support for MySQL 5.5 slow query logs

--- modules/mysql.lua
+++ modules/mysql.lua
@@ -16,6 +16,8 @@ local sep       = l.P("\n")
 local line      = (l.P(1) - sep)^0 * sep
 local float     = l.digit^1 * "." * l.digit^1
 
+local time_5_5      = l.P"# Time: " * l.digit^1 * l.space * l.digit * l.digit * ":" * l.digit * l.digit * ":" * l.digit * l.digit * sep
+
 local ip_address    = ip.v4 + ip.v6
 local user_name     = l.alpha^1 * "[" * l.alpha^1 * "]"
 local host_name     = l.alpha^0 * l.space^0 * "[" * l.Cg(ip_address^0, "Hostname") * "]"
@@ -33,7 +35,8 @@ local rows_sent     = l.P"Rows_sent: " * l.Cg(l.digit^1 / tonumber, "Rows_sent")
 local rows_examined = l.P"Rows_examined: " * l.Cg(l.digit^1 / tonumber, "Rows_examined")
 local rows_affected = l.P"Rows_affected: " * l.Cg(l.digit^1 / tonumber, "Rows_affected")
 local rows_read     = l.P"Rows_read: " * l.Cg(l.digit^1 / tonumber, "Rows_read")
-local query         = query_time * space * lock_time * space * rows_sent * space * rows_examined * space * rows_affected * space * rows_read * sep
+local query_5_1     = query_time * space * lock_time * space * rows_sent * space * rows_examined * space * rows_affected * space * rows_read * sep
+local query_5_5     = query_time * space * lock_time * space * rows_sent * space * rows_examined * sep
 
 local bytes_sent        = l.P"# Bytes_sent: " * l.Cg(l.Ct(l.Cg(l.digit^1 / tonumber, "value") * l.Cg(l.Cc"B", "representation")), "Bytes_sent")
 local tmp_tables        = l.P"Tmp_tables: " * l.Cg(l.digit^1 / tonumber, "Tmp_tables")
@@ -46,6 +49,7 @@ local use_db    = l.P"use " * line
 local timestamp = l.P"SET timestamp=" * l.Cg((l.digit^1 / "%0000000000"), "Timestamp") * line
 local sql       = l.Cg((l.P(1))^0, "Payload")
 
-slow_query_grammar = l.Ct(user * l.Cg(l.Ct(thread * query * bytes), "Fields") * inno_db^0 * use_db^0 * timestamp * sql)
+slow_query_grammar_5_1 = l.Ct(user * l.Cg(l.Ct(thread * query_5_1 * bytes), "Fields") * inno_db^0 * use_db^0 * timestamp * sql)
+slow_query_grammar_5_5 = l.Ct(time_5_5 * user * l.Cg(l.Ct(query_5_5), "Fields") * timestamp * sql)
 
 return M
