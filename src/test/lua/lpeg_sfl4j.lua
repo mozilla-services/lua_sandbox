bc8181a6d34e5b58ce140ac58ba4ad1a2e599cfe
Curt Micol <asenchi+github@asenchi.com>
2014-12-09 21:00:40 +0000

    Lets create better tests

--- src/test/lua/lpeg_sfl4j.lua
+++ src/test/lua/lpeg_sfl4j.lua
@@ -1,11 +1,5 @@
 local sfl4j = require("sfl4j")
 
-local test_log_events = {
-[[
-ERROR [2014-11-21 16:35:59,501] com.domain.client.jobs.OutgoingQueue: Error handling output file with job job-name
-]]
-}
-
 local fields = {
     {"Level", "ERROR"},
     {"Timestamp", ""},
@@ -13,19 +7,14 @@ local fields = {
     {"ErrorMessage", "Error handling output file with job job-name"}
 }
 
-local function validate(fields, t)
-    for i, v in ipairs(fields) do
-    end
-    return false
+local function single_logevent()
+    local log = 'ERROR [2014-11-21 16:35:59,501] com.domain.client.jobs.OutgoingQueue: Error handling output file with job job-name'
+    local fields = slf4j.logevent_grammar:match(log)
+    assert(fields.Level == "ERROR", fields.Level)
 end
 
-local function mainlogevent()
-    local t = sfl4j.logevent_grammar:match(test_log_events[1])
-    validate(fields, t)
-end
+function process()
+    single_logevent()
 
-function process(tc)
-    mainlogevent()
-
-    return 1
+    return 0
 end
