f96b4495929219a886c3611b0ac85391c6e07b9a
Curt Micol <asenchi+github@asenchi.com>
2014-12-09 20:04:51 +0000

    Add sfl4j.lua and tests

---
+++ modules/sfl4j.lua
@@ -0,0 +1,71 @@
+-- Using this as a reference: https://github.banksimple.com/gist/sjl/92b4b5750739a46d38a7
+--
+
+-- imports
+local l = require "lpeg"
+local dt = require "dt"
+local tonumber = tonumber
+
+l.locale(l)
+
+local M = {}
+setfenv(1, M) -- Remove external access to contain everything in the module
+
+local space  = l.space^1
+local sep    = l.P"\n"
+local level  = l.alpha^5 -- Log level (5 characters)
+local brack  = l.S("[]") -- [ and ]
+local colon  = l.S(":")  -- :
+local class  = (l.P(1) - colon)^1 -- com.domain.client.jobs.OutgoingQueue
+local line   = (l.P(1) - sep)^0 * sep
+
+-- Example: ERROR [2014-11-21 16:35:59,501] com.domain.client.jobs.OutgoingQueue: Error handling output file with job job-name
+local logline = l.Cg(level, "Level")         -- ERROR
+              * space
+              * brack
+              * l.Cg(timestamp, "Timestamp") -- [2014-11-21 16:35:59,501]
+              * brack
+              * space
+              * l.Cg(class, "class")         -- com.domain.client.jobs.OutgoingQueue
+              * colon
+              * space
+              * l.Cg(line, "ErrorMessage")                   -- Error handling output...
+
+-- TODO: Multiline, need to figure out how to capture the entire stack in a table
+-- -- Example: ! com.domain.inet.ftp.Exception: java.io.IOException: FAILED
+-- local stackline = l.P"!"
+--                 * space
+--                 * line   -- com.domain.inet.ftp.Exception
+-- 
+-- -- Example: ! at com.domain.inet.ftp.TransferMode.upload(Unknown Source)
+-- local stackatline = l.P"!"
+--                   * space
+--                   * l.P"at"
+--                   * space
+--                   * line    -- com.domain.inet.ftp.Exception
+-- 
+-- -- Matches entire line, use this to gather stuff we don't parse properly
+-- local miscline = line
+
+-- local logevent = logline
+--                * stackline
+--                * stackatline
+--                * miscline
+--                * sep
+
+local slf4j_severity = l.digit^1 / tonumber
+
+local slf4j_log_levels_text = (
+  (l.P"fatal"   + "FATAL")      / "6"
++ (l.P"error"   + "ERROR")      / "5"
++ (l.P"warn"    + "WARN")       / "4"
++ (l.P"info"    + "INFO")       / "3"
++ (l.P"debug"   + "DEBUG")      / "2"
++ (l.P"trace"   + "TRACE")      / "1"
+) / tonumber
+
+-- TODO: Map slf4j to standard log levels for "Severity"
+
+logevent_grammar = l.Ct(logline)
+
+return M
---
+++ src/test/lua/lpeg_sfl4j.lua
@@ -0,0 +1,32 @@
+local sfl4j = require("sfl4j")
+
+local test_log_events = {
+[[
+ERROR [2014-11-21 16:35:59,501] com.domain.client.jobs.OutgoingQueue: Error handling output file with job job-name
+]]
+}
+
+local fields = {
+    {"Level", "ERROR"},
+    {"Timestamp", ""},
+    {"Class", "com.domain.client.jobs.OutgoingQueue"},
+    {"ErrorMessage", "Error handling output file with job job-name"}
+}
+
+local function validate(fields, t)
+    for i, v in ipairs(fields) do
+    end
+    return false
+end
+
+local function mainlogevent()
+    local t = sfl4j.logevent_grammar:match(test_log_events[1])
+    if not t then return error("no match") end
+    validate(fields, t)
+end
+
+function process(tc)
+    mainlogevent()
+
+    return 0
+end
