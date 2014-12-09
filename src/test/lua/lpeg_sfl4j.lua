f56439e80d73e59cd050b911cb3c358a141b31da
Curt Micol <asenchi+github@asenchi.com>
2014-12-09 21:57:14 +0000

    Cleanup and idiomatic structure

--- src/test/lua/lpeg_sfl4j.lua
+++ src/test/lua/lpeg_sfl4j.lua
@@ -1,20 +1,23 @@
 local sfl4j = require("sfl4j")
 
-local fields = {
-    {"Level", "ERROR"},
-    {"Timestamp", "2014-11-21 16:35:59,501"},
-    {"Class", "com.domain.client.jobs.OutgoingQueue"},
-    {"ErrorMessage", "Error handling output file with job job-name"}
+local single_line_log = 'ERROR [2014-11-21 16:35:59,501] com.domain.client.jobs.OutgoingQueue: Error handling output file with job job-name\n'
+local single_line_test_fields = {
+    Level        = "ERROR",
+    Timestamp    = "2014-11-21 16:35:59,501",
+    Class        = "com.domain.client.jobs.OutgoingQueue",
+    ErrorMessage = "Error handling output file with job job-name"
 }
 
 local function single_logevent()
-    local log = 'ERROR [2014-11-21 16:35:59,501] com.domain.client.jobs.OutgoingQueue: Error handling output file with job job-name\n'
-    local fields = sfl4j.logevent_grammar:match(log)
-    if not fields then error("match didn't work " .. log) end
-    assert(fields.Level == "ERROR", fields.Level)
-    assert(fields.Timestamp == "2014-11-21 16:35:59,501", fields.Timestamp)
-    assert(fields.Class == "com.domain.client.jobs.OutgoingQueue", fields.Class)
-    assert(fields.ErrorMessage == "Error handling output file with job job-name", fields.ErrorMessage)
+    local fields = sfl4j.logevent_grammar:match(single_line_log)
+    if not fields then error("match didn't work " .. single_line_log) end
+    assert(fields.Level == single_line_test_fields.Level, fields.Level)
+    assert(fields.Timestamp == single_line_test_fields.Timestamp, fields.Timestamp)
+    assert(fields.Class == single_line_test_fields.Class, fields.Class)
+    assert(fields.ErrorMessage == single_line_test_fields.ErrorMessage, fields.ErrorMessage)
+end
+
+local function mult_line_logevent()
 end
 
 function process()
