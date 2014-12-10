a1abf5f62bdcae50673659cdcc1ddc296635ab7b
Curt Micol <asenchi+github@asenchi.com>
2014-12-10 20:26:43 +0000

    Cleanup and simplify

--- modules/sfl4j.lua
+++ modules/sfl4j.lua
@@ -13,32 +13,24 @@ setfenv(1, M) -- Remove external access to contain everything in the module
 local space     = l.space^1
 local sep       = l.P"\n"
 local level     = l.alpha^5 -- Log level (5 characters)
-local lbrack    = l.S("[")
-local rbrack    = l.S("]")
 local timestamp = (l.P(1) - rbrack)^0
-local colon     = l.S(":")  -- :
 local class     = (l.P(1) - colon)^1 -- com.domain.client.jobs.OutgoingQueue
 local msg       = (l.P(1) - sep)^0
 local line      = (l.P(1) - sep)^0 * sep
 
 -- Example: ERROR [2014-11-21 16:35:59,501] com.domain.client.jobs.OutgoingQueue: Error handling output file with job job-name
 local logline = l.Cg(level, "Level")         -- ERROR
-              * space * lbrack
+              * space * l.P("[")
               * l.Cg(timestamp, "Timestamp") -- 2014-11-21 16:35:59,501
-              * rbrack * space
-              * l.Cg(class, "Class")    -- com.domain.client.jobs.OutgoingQueue
-              * colon * space
-              * l.Cg(msg, "Message")    -- Error handling output...
+              * l.P("]") * space
+              * l.Cg(class, "Class")         -- com.domain.client.jobs.OutgoingQueue
+              * l.P(":") * space
+              * l.Cg(msg, "Message")         -- Error handling output...
 
--- TODO: Multiline, need to figure out how to capture the entire stack in a table
 -- Example: ! java.net.SocketTimeoutException: Read timed out
 local stackline = l.P"!" * space * line
 -- Example: ! at com.domain.inet.ftp.TransferMode.upload(Unknown Source)
 local stackatline = l.P"!" * space * l.P"at" * space * line
-
-
--- Match line, use this to gather stuff we don't parse properly
--- local misc = line
 
 local logevent = logline * sep * l.Cg(stackline^0 * stackatline^0 * line^0, "Stacktrace")
 
