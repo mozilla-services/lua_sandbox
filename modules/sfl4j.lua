a6a661e853ee111643160ac62d634b9f264175f3
Curt Micol <asenchi+github@asenchi.com>
2014-12-19 19:17:15 +0000

    Don't use literal function calls

--- modules/sfl4j.lua
+++ modules/sfl4j.lua
@@ -10,7 +10,7 @@ setfenv(1, M) -- Remove external access to contain everything in the module
 
 local space     = l.space^1
 local sep       = l.P"\n"
-local class     = (l.P(1) - l.P(":"))^1 -- com.domain.client.jobs.OutgoingQueue
+local class     = (l.P(1) - ":")^1 -- com.domain.client.jobs.OutgoingQueue
 local msg       = (l.P(1) - sep)^0
 local line      = (l.P(1) - sep)^0 * sep
 
@@ -32,22 +32,22 @@ local sfl4j_levels = l.Cg((
 + (l.P"FATAL" + "fatal") / "0")
 / tonumber, "severity")
 
-function convert_to_frac(sec)
+local function convert_to_frac(sec)
     return tonumber("0." .. sec)
 end
 
 -- Example: 2014-11-21 16:35:59,501
 local time_secfrac = l.Cg(l.digit^3 / convert_to_frac, "sec_frac")
-local partial_time = dt.time_hour * l.P":" * dt.time_minute * l.P":" * dt.time_second * l.P"," * time_secfrac^-1
+local partial_time = dt.time_hour * ":" * dt.time_minute * ":" * dt.time_second * "," * time_secfrac^-1
 local sfl4j_datetime = l.Ct(dt.rfc3339_full_date * space * partial_time) / dt.time_to_ns
 
 -- Example: ERROR [2014-11-21 16:35:59,501] com.domain.client.jobs.OutgoingQueue: Error handling output file with job job-name
 local logline = sfl4j_levels                 -- ERROR
-              * space * l.P("[")
+              * space * "["
               * l.Cg(sfl4j_datetime, "timestamp") -- 2014-11-21 16:35:59,501
-              * l.P("]") * space
+              * "]" * space
               * l.Cg(class, "class")         -- com.domain.client.jobs.OutgoingQueue
-              * l.P(":") * space
+              * ":" * space
               * l.Cg(msg, "message")         -- Error handling output...
 
 -- A representation of a full log event
