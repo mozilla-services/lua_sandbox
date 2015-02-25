f9ad136db6d9efb8d03a7f1f68e277e9089581f4
Mike Trinkala <trink@acm.org>
2015-02-25 09:52:33 -0800

    Issue #74 decode_message should throw an error instead of returning nil

--- src/test/lua/decode_message.lua
+++ src/test/lua/decode_message.lua
@@ -1,11 +1,11 @@
 -- minimal message, required fields only
 local test = "\010\016\233\213\137\149\106\254\064\066\175\098\058\163\017\067\202\068\016\000"
-local msg = assert(decode_message(test))
+local msg = decode_message(test)
 assert(msg.Timestamp == 0)
 
 -- message headers only
 local test = "\010\016\233\213\137\149\106\254\064\066\175\098\058\163\017\067\202\068\016\128\148\235\220\003\026\004type\034\006logger\040\009\050\007payload\058\011env_version\074\008hostname"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(msg.Type == "type")
 assert(msg.Logger == "logger")
@@ -16,7 +16,7 @@ assert(msg.Severity == 9)
 
 -- repeated unpacked doubles
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\027\010\005count\016\003\057\000\000\000\000\000\000\240\063\057\000\000\000\000\000\000\240\063"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(msg.Fields[1].name == "count")
 assert(msg.Fields[1].value_type == 3)
@@ -26,7 +26,7 @@ assert(msg.Fields[1].value[2] == 1)
 
 -- repeated packed doubles
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\027\010\005count\016\003\058\016\000\000\000\000\000\000\240\063\000\000\000\000\000\000\240\063"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(msg.Fields[1].name == "count")
 assert(msg.Fields[1].value_type == 3)
@@ -36,7 +36,7 @@ assert(msg.Fields[1].value[2] == 1)
 
 -- repeated unpacked ints
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\013\010\005count\016\002\048\001\048\001"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(msg.Fields[1].name == "count")
 assert(msg.Fields[1].value_type == 2)
@@ -46,7 +46,7 @@ assert(msg.Fields[1].value[2] == 1)
 
 -- repeated packed ints
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\013\010\005count\016\002\050\002\001\001"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(msg.Fields[1].name == "count")
 assert(msg.Fields[1].value_type == 2)
@@ -56,7 +56,7 @@ assert(msg.Fields[1].value[2] == 1)
 
 -- repeated unpacked bools
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\013\010\005count\016\004\064\001\064\000"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(msg.Fields[1].name == "count")
 assert(msg.Fields[1].value_type == 4)
@@ -66,7 +66,7 @@ assert(msg.Fields[1].value[2] == false)
 
 -- repeated packed bools
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\013\010\005count\016\004\066\002\001\000"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(msg.Fields[1].name == "count")
 assert(msg.Fields[1].value_type == 4)
@@ -76,7 +76,7 @@ assert(msg.Fields[1].value[2] == false)
 
 -- repeated strings
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\023\010\005names\016\000\034\002s1\034\002s2\026\004keys"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(msg.Fields[1].name == "names")
 assert(msg.Fields[1].representation == "keys")
@@ -87,7 +87,7 @@ assert(msg.Fields[1].value[2] == "s2")
 
 -- repeated bytes
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\023\010\005names\016\001\042\002s1\042\002s2\026\004keys"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(msg.Fields[1].name == "names")
 assert(msg.Fields[1].representation == "keys")
@@ -98,7 +98,7 @@ assert(msg.Fields[1].value[2] == "s2")
 
 -- benchmark message
 test = "\010\016\096\006\214\155\119\188\078\023\172\076\081\127\129\143\250\040\016\128\148\235\220\003\082\019\010\006\110\117\109\098\101\114\016\003\057\000\000\000\000\000\000\240\063\082\044\010\007\110\117\109\098\101\114\115\016\003\026\005\099\111\117\110\116\058\024\000\000\000\000\000\000\240\063\000\000\000\000\000\000\000\064\000\000\000\000\000\000\008\064\082\014\010\005\098\111\111\108\115\016\004\066\003\001\000\000\082\010\010\004\098\111\111\108\016\004\064\001\082\016\010\006\115\116\114\105\110\103\034\006\115\116\114\105\110\103\082\021\010\007\115\116\114\105\110\103\115\034\002\115\049\034\002\115\050\034\002\115\051"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == 1e9)
 assert(#msg.Fields == 6)
 
@@ -137,14 +137,14 @@ assert(msg.Fields[6].value[3] =="s3")
 
 -- test negative varint headers
 test = "\010\016\243\083\052\234\016\052\066\236\160\084\236\003\227\231\170\203\016\255\255\255\255\255\255\255\255\255\001\040\255\255\255\255\255\255\255\255\255\001\064\255\255\255\255\255\255\255\255\255\001"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Timestamp == -1)
 assert(msg.Pid == -1)
 assert(msg.Severity == -1)
 
 -- multi byte length
 test = "\010\016\104\183\062\106\120\227\073\248\173\168\122\019\057\236\124\025\016\128\148\235\220\003\082\141\001\010\006\115\116\114\105\110\103\034\130\001\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057\048\049\050\051\052\053\054\055\056\057"
-msg = assert(decode_message(test))
+msg = decode_message(test)
 assert(msg.Fields[1].value[1] == "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789")
 
 
@@ -154,48 +154,48 @@ Parse Errors
 
 -- too short
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132"
-msg, err = decode_message(test)
-assert(not msg)
-assert(err == "invalid message, too short", err)
+ok, msg = pcall(decode_message, test)
+assert(not ok)
+assert(msg == "invalid message, too short", msg)
 
 -- no uuid
 test = "\016\128\148\235\220\003\082\023\010\005names\016\001\042\002s1\042\002s2\026\004keys"
-msg, err = decode_message(test)
-assert(not msg)
-assert(err == "missing required field uuid: not found timestamp: found", err)
+ok, msg = pcall(decode_message, test)
+assert(not ok)
+assert(msg == "missing required field uuid: not found timestamp: found", msg)
 
 -- no timestamp
 local test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\082\023\010\005names\016\001\042\002s1\042\002s2\026\004keys"
-msg, err = decode_message(test)
-assert(not msg)
-assert(err == "missing required field uuid: found timestamp: not found", err)
+ok, msg = pcall(decode_message, test)
+assert(not ok)
+assert(msg == "missing required field uuid: found timestamp: not found", msg)
 
 -- missing field name
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\020\016\003\057\000\000\000\000\000\000\240\063\057\000\000\000\000\000\000\240\063"
-msg, err = decode_message(test)
-assert(not msg)
-assert(err == "error in tag: 10 wiretype: 2 offset: 24", err)
+ok, msg = pcall(decode_message, test)
+assert(not ok)
+assert(msg == "error in tag: 10 wiretype: 2 offset: 24", msg)
 
 -- random string
 test = "this is a test item over twenty bytes long"
-msg, err = decode_message(test)
-assert(not msg)
-assert(err == "error in tag: 14 wiretype: 4 offset: 0", err)
+ok, msg = pcall(decode_message, test)
+assert(not ok)
+assert(msg == "error in tag: 14 wiretype: 4 offset: 0", msg)
 
 -- repeated packed with ints invalid length
 test = "\010\016\111\021\235\034\090\107\077\120\169\175\058\232\153\002\231\132\016\128\148\235\220\003\082\013\010\005count\016\002\050\003\001\001"
-msg, err = decode_message(test)
-assert(not msg)
-assert(err == "error in tag: 10 wiretype: 2 offset: 24", err)
+ok, msg = pcall(decode_message, test)
+assert(not ok)
+assert(msg == "error in tag: 10 wiretype: 2 offset: 24", msg)
 
 -- invalid timestamp varint encoding
 local test = "\010\016\233\213\137\149\106\254\064\066\175\098\058\163\017\067\202\068\016\128\148\235\220\220\220\220\220\220\220"
-msg, err = decode_message(test)
-assert(not msg)
-assert(err == "error in tag: 2 wiretype: 0 offset: 18", err)
+ok, msg = pcall(decode_message, test)
+assert(not ok)
+assert(msg == "error in tag: 2 wiretype: 0 offset: 18", msg)
 
 -- invalid length encoding
 local test = "\010\016\233\213\137\149\106\254\064\066\175\098\058\163\017\067\202\068\016\128\148\235\220\003\026\128\148\235\220\220\220\220\220\220\220type"
-msg, err = decode_message(test)
-assert(not msg)
-assert(err == "error in tag: 3 wiretype: 2 offset: 24", err)
+ok, msg = pcall(decode_message, test)
+assert(not ok)
+assert(msg == "error in tag: 3 wiretype: 2 offset: 24", msg)
