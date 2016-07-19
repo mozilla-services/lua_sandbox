fe9d26eb6d5f7e44efaa1d0353098e4f9897f3aa
Mike Trinkala <trink@acm.org>
2016-07-19 13:28:14 -0700

    Fix up the build/install on RedHat
    - Move the cmake configuration file to a directory we own
      - prevent possible RPM install directory permission conflicts
    - remove the indentation causing a build warning -> error on FC24

--- src/lua/ltablib.c
+++ src/lua/ltablib.c
@@ -16,7 +16,7 @@
 #include "lualib.h"
 
 
-#define aux_getn(L,n)	(luaL_checktype(L, n, LUA_TTABLE), luaL_getn(L, n))
+#define aux_getn(L,n) (luaL_checktype(L, n, LUA_TTABLE), luaL_getn(L, n))
 
 
 static int foreachi (lua_State *L) {
@@ -137,7 +137,7 @@ static void addfield (lua_State *L, luaL_Buffer *b, int i) {
   if (!lua_isstring(L, -1))
     luaL_error(L, "invalid value (%s) at index %d in table for "
                   LUA_QL("concat"), luaL_typename(L, -1), i);
-    luaL_addvalue(b);
+  luaL_addvalue(b);
 }
 
 
