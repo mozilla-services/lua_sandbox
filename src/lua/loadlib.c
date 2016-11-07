2d4c568641a60800e515a42e93fc9f2e1cf33c0a
Mathieu Parent <math.parent@gmail.com>
2016-11-07 13:04:46 +0100

    Apply missing changes before 856593a47aae610fab5a6023b88f558a58becfb6

--- src/lua/loadlib.c
+++ src/lua/loadlib.c
@@ -74,7 +74,8 @@ static void *ll_load (lua_State *L, const char *path) {
 
 
 static lua_CFunction ll_sym (lua_State *L, void *lib, const char *sym) {
-  lua_CFunction f = (lua_CFunction)dlsym(lib, sym);
+  lua_CFunction f;
+  *(void **)(&f)= dlsym(lib, sym);
   if (f == NULL) lua_pushstring(L, dlerror());
   return f;
 }
@@ -287,22 +288,6 @@ static int ll_loadfunc (lua_State *L, const char *path, const char *sym) {
     return 0;  /* return function */
   }
 }
-
-
-static int ll_loadlib (lua_State *L) {
-  const char *path = luaL_checkstring(L, 1);
-  const char *init = luaL_checkstring(L, 2);
-  int stat = ll_loadfunc(L, path, init);
-  if (stat == 0)  /* no errors? */
-    return 1;  /* return the loaded function */
-  else {  /* error; error message is on stack top */
-    lua_pushnil(L);
-    lua_insert(L, -2);
-    lua_pushstring(L, (stat == ERRLIB) ?  LIB_FAIL : "init");
-    return 3;  /* return nil, error message, and where */
-  }
-}
-
 
 
 /*
